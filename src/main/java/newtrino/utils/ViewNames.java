d154d16044f31c7dc8a6fc038d373f36de5f4894
MCS <mcs@o2.com>
2015-02-12 20:15:36 +0000

    Initial Commit

---
+++ src/main/java/newtrino/utils/ViewNames.java
@@ -0,0 +1,7 @@
+package newtrino.utils;
+
+public interface ViewNames {
+    String FOLDER_FOOD = "food/";
+    String FOOD_HOME = FOLDER_FOOD+"FoodHome";
+    String ADD_FOOD = FOLDER_FOOD+"AddFood";
+}
